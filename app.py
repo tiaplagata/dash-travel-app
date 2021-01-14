57f6c611bf32019467a07e91afd662dbf468904a
Tia Plagata <tiaplagata@gmail.com>
2021-01-14 08:36:01 -0500

    updates

--- app.py
+++ app.py
@@ -18,16 +18,36 @@ import pandas as pd
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
 
-# server = flask.Flask(__name__)
-# server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 
 app = dash.Dash(__name__)
 server = app.server
 
 # Put your Dash code here
 
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+
+# The app layout
+app.layout = html.Div(children=[
+    html.H1(children='Where should I travel?'),
+
+    html.Div(children='When traveling becomes a normal passtime again, where should you go? What do you want to do while on vacation?'),
+
+    dcc.Input(id='input_bar',value='ex. I want to go snorkeling and do yoga on the beach',
+              placeholder='MSFT,AAPL',
+              style={'width': '90%', 'height': 50})
+
+])
 
 # Run the Dash app
 if __name__ == '__main__':
-#     app.server.run(debug=True, threaded=True)
-    app.run_server(debug=True)
+    app.server.run(debug=True, threaded=True)
+#     app.run_server(debug=True)
